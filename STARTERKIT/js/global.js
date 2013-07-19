3659d95871c331c897bc4c6b997fe9c242ec64d6
eric paul <multipurposegeek@gmail.com>
2013-07-18 17:39:42 -0700

    More structuring.

---
+++ STARTERKIT/js/global.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Global script for STARTERKIT.
+ *
+ */
+
+(function ($, Drupal, window, document, undefined) {
+
+
+// Place your code here.
+
+
+})(jQuery, Drupal, this, this.document);
