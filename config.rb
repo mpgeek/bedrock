916b37570facd8c91bccc00ca3ce0d5790165242
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-07-26 14:02:18 -0700

    Rename 'script' to 'scripts' to parallel 'images'.

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "script"
+javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
