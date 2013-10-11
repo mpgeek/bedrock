8185e29d47f280a13d644cb7a0977378dcb509e7
eric paul <multipurposegeek@gmail.com>
2013-10-11 11:36:12 -0700

    Normalize sass config.

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'aurora'
 
 # Set this to the root of your project when deployed:
 http_path = "/sites/all/themes/bedrock"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "scripts"
 fonts_dir = "stylesheets/fonts"
 
 # Change this to :production when ready to deploy the CSS to the live server.
@@ -14,13 +14,17 @@ fonts_dir = "stylesheets/fonts"
 environment = :development
 #environment = :production
 
-# To enable relative paths to assets via compass helper functions. Since Drupal themes can be installed in multiple locations, we shouldn't need to worry about the absolute path to the theme from the server root.
+# To enable relative paths to assets via compass helper functions. Since Drupal
+#themes can be installed in multiple locations, we shouldn't need to worry about
+# the absolute path to the theme from the server root.
 relative_assets = true
 
-# To enable debugging comments that display the original location of your selectors. Comment:
+# To enable debugging comments that display the original location of your
+# selectors. Comment:
 line_comments = false
 
-# In development, we can turn on the debug_info to use with FireSass or Chrome Web Inspector. Uncomment:
+# In development, we can turn on the debug_info to use with FireSass or Chrome
+# Web Inspector. Uncomment:
 # debug = true
 
 
@@ -31,7 +35,8 @@ line_comments = false
 # Disable cache busting on image assets
 asset_cache_buster :none
 
-# You can select your preferred output style here (can be overridden via the command line):
+# You can select your preferred output style here
+# (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = (environment == :development) ? :expanded : :compressed
 
